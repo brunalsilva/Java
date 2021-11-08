e8abd6bcab31b9572b93c9dc6887a497ec614136
Bruna Silva <beyss21@gmail.com>
2021-11-08 18:10:06 -0300

    Inserindo o enunciado dos exercicios

--- Operadores Logicos/Ex2.java
+++ Operadores Logicos/Ex2.java
@@ -5,6 +5,9 @@ public class Ex2 {
 
 	public static void main(String[] args) {
 		
+		/*Faça um sistema que leia a idade de uma pessoa expressa em dias e mostre-a
+		expressa em anos, meses e dias.*/
+		
 		Scanner leia = new Scanner(System.in);
 		
 		System.out.println("Insira a sua idade somente em dias: ");
